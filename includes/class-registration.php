1e61f7d7011d82bf671ccc3ad214814957c6b2b2
Thom Bottram <thom_bottram@live.nl>
2021-12-03 11:36:18 +0100

    change version

--- includes/class-registration.php
+++ includes/class-registration.php
@@ -69,7 +69,7 @@ class registration {
 	public function __construct() {
 
 		$this->registration = 'registration';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
