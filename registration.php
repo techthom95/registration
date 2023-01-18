f2aae949e3813ea721732a6971b7634f11adfbac
Thom Bottram <thom@techthom.nl>
2023-01-18 13:12:28 +0100

    Update Main

--- registration.php
+++ registration.php
@@ -13,7 +13,7 @@
  * Description:       Dynamische inschrijf formulieren
  * Version:           1.0.1
  * Author:            Thom Bottram
- * Author URI:        http://www.techthom.nl/
+ * Author URI:        https://www.techthom.nl/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       registration
